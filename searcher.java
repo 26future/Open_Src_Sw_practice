f56b57d6c781d2e009081b0cd361e22a49f71586
Mirae <future26@konkuk.ac.kr>
2022-04-15 11:16:09 +0900

    feature 브랜치 수정

--- searcher.java
+++ searcher.java
@@ -1,5 +1,8 @@
 package practice;
 
 public class searcher {
-
+	public static double InnerProduct(double doc_weight, int query_weight) {
+		double inner_product = doc_weight * query_weight;
+		return inner_product;
+}
 }
